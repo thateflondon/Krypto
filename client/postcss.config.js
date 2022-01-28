b0bf1e1f61596abc9716f16afb7aa30e97d5b795
Jerry Frederic <jfpl.dev@gmail.com>
2022-01-28 14:34:51 +0100

    build(all): set client

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,10 +1,3 @@
-//---Initial--- set back if needed
-// const tailwindcss = require("tailwindcss");
-// module.exports = {
-//   plugins: [tailwindcss("./tailwind.config.js"), require("autoprefixer")],
-// };
-
-//From tailwindcss guide
 module.exports = {
   plugins: {
     tailwindcss: {},
