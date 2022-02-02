89da1db56a59b5cf535fe2e2f31201e3430d9039
Jerry Frederic <jfpl.dev@gmail.com>
2022-02-02 02:23:22 +0100

    feat(app): create form's input element props

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -14,7 +14,16 @@ const commonStyles =
 //   />
 // );
 
-const Input = ({ placeholder }) => <input placeholder={placeholder} />;
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+  <input
+    placeholder={placeholder}
+    type={type}
+    value={value}
+    onChange={(e) => handleChange(e, name)}
+    step="0.0001"
+    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism "
+  />
+);  
 
 const Welcome = () => {
   // helps to connect wallet
